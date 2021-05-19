77890613b06b3502a22402129c172345e63443b0
倉園あずさ <kuraenazusa@kuraenazusanoMacBook-Air.local>
2021-05-19 21:31:09 +0900

    指摘１回目の修正完了

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@
 // 前提：3回まで繰り返す
 
 let count = 1;
+let flg = true;
 
-while (count <= 3) {
+while (count <= 3 && flg　=== true ) {
   let name = prompt('日本の首都は？');
   if (name === '東京') {
     alert('正解です！');
+    fjg = false;
   } else {
     alert('不正解です！');
   }
