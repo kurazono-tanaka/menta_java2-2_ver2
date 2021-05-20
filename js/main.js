1c19c93789b2d97638b05538522fc59be09062fe
倉園あずさ <kuraenazusa@kuraenazusanoMacBook-Air.local>
2021-05-21 00:33:43 +0900

    指摘２回目(break文への書き換え)の修正完了

--- js/main.js
+++ js/main.js
@@ -3,13 +3,12 @@
 // 前提：3回まで繰り返す
 
 let count = 1;
-let flg = true;
 
-while (count <= 3 && flg　=== true ) {
+while (count <= 3) {
   let name = prompt('日本の首都は？');
   if (name === '東京') {
     alert('正解です！');
-    flg = false;
+    break;
   } else {
     alert('不正解です！');
   }
